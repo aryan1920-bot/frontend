39316817f6fc79532ba9c3f19ea58d72be0d3a0d
Aryan <78062772+aryan1920-bot@users.noreply.github.com>
2023-06-06 14:44:58 +0530

    Initial commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>NOTE</h1>
+      <p>*Enter your note*</p>
+    </div>
+  );
+}
+
+export default Notes;
